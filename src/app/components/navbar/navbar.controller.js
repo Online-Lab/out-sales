ee38f732fccab087ef78b84afdf6663294134bcd
fsanano <memonanofsa@gmail.com>
2015-06-15 17:56:08 +0300

    clean project

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('outSales')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('outSales')
+  .controller('MainCtrl', function ($scope) {
+      $scope.some = [];
+  });
